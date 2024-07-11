11fa48c30c2660589be1c3d431e63fa303dccf2b
Tuan Nguyen AMIT <nguyentuanamit@gmail.com>
2024-07-12 00:30:39 +0700

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ import userController from "../controllers/userController";
 const router = express.Router();
 
 router.post("/create", userController.createUser);
+router.delete("/removecartitem", userController.removeProductFromCart);
 router.put("/reset-balance/:id", userController.resetBalance);
 router.get("/with-balance", userController.getUserBalances);
 router.put("/:id", userController.updateUser);
@@ -12,7 +13,6 @@ router.delete("/:id", userController.deleteUser);
 router.post("/login", userController.login);
 router.get("/:id", userController.getUserById);
 router.post("/addtocart", userController.addProductToCart);
-router.delete("/removecartitem", userController.removeProductFromCart);
 router.patch("/updateAdmin/:id", userController.updateIsAdmin);
 
 export default router;
